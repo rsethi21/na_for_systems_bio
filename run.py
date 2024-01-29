28848f8941c6a395420cb05568f7af379dd65bb2
Rohan Sethi <rsethi1@luc.edu>
2024-01-29 16:08:03 -0600

    added example run

--- run.py
+++ run.py
@@ -21,25 +21,33 @@ parser.add_argument("-a", "--arguments", help="fitting algorithm arguments json 
 parser.add_argument("-o", "--output", help="path to the output directory to save output files", required=False, default=".")
 parser.add_argument("-p", "--parameters", help="pretrained parameters json file", required=False, default=None)
 parser.add_argument("-m", "--multi", help="number of processers to use for fitting", type=int, required=False, default=1)
+parser.add_argument("-n", "--number", help="number of random samples for area plot", type=int, required=False, default=500)
+parser.add_argument("-t", "--toplot", help="list of substrates to plot", nargs="*", required=False, default=[])
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    
+   
+    # parse and create all necessary objects for creating a network
     rates = parse_rates(args.rates)
     substrates = parse_substrates(args.substrates, rates)
     interactions = parse_interactions(args.interactions, rates, substrates)
     
+    # create a new instance of a network
     network = Network("example", rates, interactions, substrates)
+    
+    # visualize network ordinary differential equations
     time = np.array([i for i in range(500)])
     derivatives = network.get_representation_dydt()
     print(derivatives)
 
+    # load in pretrained parameters if any
     if args.parameters != None:
         with open(args.parameters, "r") as fitted_params:
             parameters = json.load(fitted_params)
         network.set_parameters(list(parameters.values()), list(parameters.keys()))
 
-    if args.fitting != None and args.arguments != None:
+    # train model if needed
+    if args.fitting != None or args.arguments != None:
         with open(args.fitting, "r") as file:
             fit_dictionary = json.load(file)
         with open(args.arguments, "r") as argument_file:
@@ -48,10 +56,17 @@ if __name__ == "__main__":
         with open(os.path.join(args.output, "fitted_params.json"), "w") as out_file:
             json.dump({i: r.value for i, r in network.parameters.items()}, out_file)
     
-    y, f = network.graph_distributions(time, 500, substrates_to_plot=["PI3K", "pAKT", "pPTEN", "Phagocytosis", "LPS", "HDACi"], normalize=True, path=os.path.join(args.output, "figure.png"), output_figure=True)
+    # create area plots for the network
+    y, f = network.graph_distributions(time, args.number, substrates_to_plot=["PI3K", "pAKT", "pPTEN", "Phagocytosis", "LPS", "HDACi"], normalize=True, path=os.path.join(args.output, "figure.png"), output_figure=True)
 
+    # plot fitting data to visually evaluate the fit
     plt.figure(f)
-    times = [60, 240, 245, 250, 260, 270, 300, 360, 420]
-    phagocytosis_ys = [1.0, 0.3235695, 0.35537492, 0.33414908, 0.34072799, 0.46634439, 0.44325203, 0.80413556, 1.04238109]
-    plt.scatter([60, 240, 245, 250, 260, 270, 300, 360, 420], phagocytosis_ys, s=1)
+    times = [60, 240, 245, 250, 260, 270, 300]
+    times2 = [420, 420, 420, 420]
+    values = [0.4, 0.4, 0.6, 1.1]
+    names = ["pAKT", "PIP3", "pPTEN", "GSK3B"]
+    phagocytosis_ys = [1.0, 0.3235695, 0.35537492, 0.33414908, 0.34072799, 0.46634439, 0.44325203]
+    
+    plt.scatter(times, phagocytosis_ys, s=2, color=network.colors[list(network.substrates.keys()).index("Phagocytosis")])
+    plt.scatter(times2, values, s=2, color=[network.colors[list(network.substrates.keys()).index(n)] for n in names])
     f.savefig(os.path.join(args.output, "figure_w_data.png"))
