cc127fc9a2b3a2d1b9d7840398185992909591a0
Rohan Sethi <rsethi1@luc.edu>
2024-02-05 17:54:03 -0600

    sending atp test

--- testing_atp.py
+++ testing_atp.py
@@ -46,4 +46,5 @@ if __name__ == "__main__":
         network.set_parameters(list(parameters.values()), list(parameters.keys()))
 
     # check post-training training error
+    network.substrates["ATP"].trs = [[200, 400]]
     mean_y, f = network.graph_distributions(time, args.number, substrates_to_plot=["PI3K", "pAKT", "pPTEN", "Phagocytosis", "LPS", "HDACi", "GSK3B", "LY294-002", "ATP"], normalize=True, path=os.path.join(args.output, "figure_atp.png"), output_figure=True)
