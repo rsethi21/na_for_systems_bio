a701dcd60be6b4645afbc7cb2377675bf031b58a
Rohan Sethi <rsethi1@luc.edu>
2024-02-10 04:05:04 -0600

    atp test with new parameters

--- testing_atp.py
+++ testing_atp.py
@@ -47,5 +47,4 @@ if __name__ == "__main__":
         network.set_parameters(list(parameters.values()), list(parameters.keys()))
 
     # check post-training training error
-    network.substrates["ATP"].time_ranges = [[200, 380]]
     mean_y, f = network.graph_distributions(time, args.number, substrates_to_plot=["PI3K", "pAKT", "pPTEN", "Phagocytosis", "LPS", "HDACi", "LY294-002", "ATP"], normalize=True, path=os.path.join(args.output, "figure_atp.png"), output_figure=True)
